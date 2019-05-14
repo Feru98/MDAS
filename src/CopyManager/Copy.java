951ff303fca3f41b95a6638f96d6e71d8ddb684d
Miguelo0098 <i62rarum@uco.es>
2019-05-14 16:18:06 +0200

    Added docs to class Copy

--- src/CopyManager/Copy.java
+++ src/CopyManager/Copy.java
@@ -5,20 +5,32 @@ public class Copy {
    private float _price;
    
    
-
+   /**
+    * @return the _isbn
+    */
    public String getISBN() {
 	   return _isbn;
    }
-
+   /**
+    * @param _isbn the _isbn to set
+    */
    public void setISBN(String _isbn) {
 	   this._isbn = _isbn;
    }
-
+   
+   /**
+    * @return the _price
+    */
    public float getPrice() {
 	   return _price;
    }
-
+   /**
+    * @param _price the _price to set
+    */
    public void setPrice(float _price) {
 	   this._price = _price;
    }
+   
+   
+
 }
