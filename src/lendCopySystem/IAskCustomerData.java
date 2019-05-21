d3356a9b6398642af332ea4eb0be0738d96c0846
Miguelo0098 <i62rarum@uco.es>
2019-05-21 10:35:47 +0200

    Added prototypes

--- src/lendCopySystem/IAskCustomerData.java
+++ src/lendCopySystem/IAskCustomerData.java
@@ -1,5 +1,12 @@
 package lendCopySystem;
+import InvoiceManager.Customer;
 
 public interface IAskCustomerData {
-
+	Customer createCustomer();
+	
+	boolean registerCustomer();
+	
+	Customer getCustomerData();
+	
+	boolean setCustomerData();
 }
