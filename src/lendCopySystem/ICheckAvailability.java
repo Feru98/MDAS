f20afeb71f93a41866ae7f41acbb396e0f34987b
Miguelo0098 <i62rarum@uco.es>
2019-05-22 11:21:47 +0200

    Added documentation to interface

--- src/lendCopySystem/ICheckAvailability.java
+++ src/lendCopySystem/ICheckAvailability.java
@@ -1,9 +1,24 @@
 package lendCopySystem;
+import CopyManager.SecondHandBook;
 
-import CopyManager.Copy;
 
+/**
+ * Interface to Check Availability of a copy
+ * @author Miguel Ángel Ramírez Ruiz
+ *
+ */
 public interface ICheckAvailability {
-	Copy getCopyData();
+	/**
+	 * Gets information about a Second Hand Book
+	 * @param id of the book
+	 * @return A second hand book data
+	 */
+	SecondHandBook getCopyData(String id);
 	
-	boolean checkAvailability();
+	/**
+	 * Checks the availability of a book by it's id
+	 * @param id ok the book
+	 * @return true if the book is available, false if not available or doesn't exists
+	 */
+	boolean checkAvailability(String id);
 }
