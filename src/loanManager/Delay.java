598718313ee31b5a631910839e4cdc7eaf112a82
miguelo0098 <mugui098@gmail.com>
2019-05-23 18:55:05 +0200

    Changed Date attribute to days

--- src/loanManager/Delay.java
+++ src/loanManager/Delay.java
@@ -2,23 +2,23 @@ package loanManager;
 import java.util.Date;
 
 public class Delay {
-	
-	private Date _date = new Date();
-	private int _id ; 
-	private float _punishmentAmount; 
+
+	private int _days;
+	private int _id;
+	private float _punishmentAmount;
 
 	/**
-	 * @return the _date
+	 * @return the days of delay
 	 */
-	public Date get_date() {
-		return _date;
+	public int get_days() {
+		return _days;
 	}
 
 	/**
-	 * @param _date the _date to set
+	 * @param _days the _days to set
 	 */
-	public void set_date(Date _date) {
-		this._date = _date;
+	public void set_days(int _days) {
+		this._days = _days;
 	}
 
 	/**
@@ -48,6 +48,5 @@ public class Delay {
 	public void set_punishementAmount(float _punishmentAmount) {
 		this._punishmentAmount = _punishmentAmount;
 	}
-	
 
 }
