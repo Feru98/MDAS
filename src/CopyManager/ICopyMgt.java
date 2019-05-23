f466902c2c7a6a0ebdc361eda937b7940d02520e
Feru98 <fernandomaestrec1998@gmail.com>
2019-05-23 12:55:23 +0200

    interface copy

--- src/CopyManager/ICopyMgt.java
+++ src/CopyManager/ICopyMgt.java
@@ -8,8 +8,30 @@ package CopyManager;
  */
 public interface ICopyMgt {
 	
-	public void changeCopyStatus(); 
-	public void getCopyDetails();
-	public void addCopy();
-	public void deleteCopy();
+	
+	/**
+	 * Method that gets a copy and a new status and change that copy status to the new one
+	 * @param a
+	 * @param status
+	 */
+	public void changeCopyStatus(SecondHandBook a , String status);
+	
+	/**
+	 * Method that get a copy isbn and returns the second hand book that matches it
+	 * @param id = string that is the isbn of a copy
+	 * @return the second hand book that matches the isbn or null if not found
+	 */
+	public SecondHandBook getCopyDetails(String id);
+		
+	/**
+	 * Method that receives a SecondHandBook and adds it to the array
+	 * @param aux
+	 */
+	public void addCopy(SecondHandBook aux);
+		
+	/**
+	 * Method that receives a SecondHandBook and removes it to the array
+	 * @param id
+	 */
+	public void deleteCopy(String id);
 }
